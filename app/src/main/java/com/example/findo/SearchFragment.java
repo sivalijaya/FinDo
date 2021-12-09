d20647cf3499d5cd5652f1f619712a85aaf64b1c
sivalijaya <sivalijaya@gmail.com>
2021-12-10 01:27:40 +0700

    add searchFragment

---
+++ app/src/main/java/com/example/findo/SearchFragment.java
@@ -0,0 +1,14 @@
+package com.example.findo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_search);
+    }
+}
