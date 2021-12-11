e2d7ec3b19c06883cda75c8cf8a09cc4f8808cff
sivalijaya <sivalijaya@gmail.com>
2021-12-11 18:12:12 +0700

    add camera button

--- app/src/main/java/com/example/findo/HomeActivity.java
+++ app/src/main/java/com/example/findo/HomeActivity.java
@@ -2,7 +2,12 @@ package com.example.findo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageButton btn_photo = findViewById(R.id.photo);
+
+        btn_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo change intent to camera
+                Intent intent = new Intent(HomeActivity.this, SplashActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
