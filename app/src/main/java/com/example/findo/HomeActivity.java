23b48363b57a54ba7557847af7f083ef80524857
sivalijaya <sivalijaya@gmail.com>
2021-12-22 21:54:54 +0700

    update code for camera

--- app/src/main/java/com/example/findo/HomeActivity.java
+++ app/src/main/java/com/example/findo/HomeActivity.java
@@ -1,15 +1,26 @@
 package com.example.findo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.RelativeLayout;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final int CAMERA_PERMISSION_CODE = 1;
+    private static final int CAMERA_REQUEST_CODE = 2;
+
+    ImageView iv_photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +28,32 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         ImageButton btn_photo = findViewById(R.id.photo);
+        iv_photo = findViewById(R.id.testphoto);
 
         btn_photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //todo change intent to camera
-                Intent intent = new Intent(HomeActivity.this, SplashActivity.class);
-                startActivity(intent);
+                if(ContextCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, CAMERA_REQUEST_CODE);
+                }else{
+                    String[] permissions = new String[] {
+                            Manifest.permission.CAMERA,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    };
+                    ActivityCompat.requestPermissions(HomeActivity.this, permissions, CAMERA_PERMISSION_CODE);
+                }
             }
         });
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK && data != null){
+            Bundle bundle = data.getExtras();
+            Bitmap finalPhoto = (Bitmap) bundle.get("data");
+            iv_photo.setImageBitmap(finalPhoto);
+        }
+    }
 }
